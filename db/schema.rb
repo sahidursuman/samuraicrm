0b5c81dcdef7af4f44c34ff3b768d0d1c535bcc9
alvinkatojr <alvinkatojr@gmail.com>
2017-04-09 00:06:21 +0300

    Adds admin attribute to user, updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404202833) do
+ActiveRecord::Schema.define(version: 20170408210052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170404202833) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "samurai_users", ["email"], name: "index_samurai_users_on_email", unique: true, using: :btree
