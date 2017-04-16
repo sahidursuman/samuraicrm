a38773d4c3396f070e6655578e76aef2bd8a8063
alvinkatojr <alvinkatojr@gmail.com>
2017-04-17 01:51:24 +0300

    Overrides current ability method

--- engines/core/app/controllers/samurai/application_controller.rb
+++ engines/core/app/controllers/samurai/application_controller.rb
@@ -1,5 +1,8 @@
 module Samurai
   class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    def current_ability
+      @current_ability ||= Samurai::Ability.new(current_user)
+    end
   end
 end
