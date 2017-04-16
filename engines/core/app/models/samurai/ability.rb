2ab3ccea163ea027709ac02f7155d6b2a5b87e53
alvinkatojr <alvinkatojr@gmail.com>
2017-04-17 02:12:51 +0300

    Ability definitions

--- engines/core/app/models/samurai/ability.rb
+++ engines/core/app/models/samurai/ability.rb
@@ -3,6 +3,12 @@ module Samurai
     include CanCan::Ability
 
     def initialize(user)
+
+      if user.admin?
+        can :manage, :all
+      else
+        can :read, :dashboard
+      end
     end
   end
 end
