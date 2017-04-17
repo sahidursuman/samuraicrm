89ed150c03518ff7f0cc0fdbe7a60c362e8f3546
alvinkatojr <alvinkatojr@gmail.com>
2017-04-17 03:35:24 +0300

    Test configurations done

--- engines/core/spec/rails_helper.rb
+++ engines/core/spec/rails_helper.rb
@@ -3,6 +3,10 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'database_cleaner'
+
+ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../')
+Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
--- engines/core/spec/spec_helper.rb
+++ engines/core/spec/spec_helper.rb
@@ -8,4 +8,19 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.include Samurai::Core::Engine.routes.url_helpers
 end
