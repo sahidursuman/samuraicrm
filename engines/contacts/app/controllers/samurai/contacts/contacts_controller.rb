37d0ab0d8fc7d82fb510307f80ed5332f090c326
alvinkatojr <alvinkatojr@gmail.com>
2017-05-01 19:34:09 +0300

    All restful actions added to the mix :P

--- engines/contacts/app/controllers/samurai/contacts/contacts_controller.rb
+++ engines/contacts/app/controllers/samurai/contacts/contacts_controller.rb
@@ -4,6 +4,39 @@ module Samurai::Contacts
   class ContactsController < ApplicationController
     before_action :set_contact, only: [:show, :edit, :update, :destroy]
 
+    def index
+      @contacts = Contact.all
+    end
+
+    def show
+    end
+
+    def new
+      @contact = Contact.new
+    end
+
+    def edit
+    end
+
+    def create
+      @contact = Contact.new(contact_params)
+      @contact.user = current_user
+
+      if @contact.save
+        redirect_to [samurai, @contact], notice: 'Contact was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    def update
+      if @contact.update(contact_params)
+        redirect_to [samurai, @contact], notice: 'Contact was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
     private
       def set_contact
         @contact = Contact.find(params[:id])
