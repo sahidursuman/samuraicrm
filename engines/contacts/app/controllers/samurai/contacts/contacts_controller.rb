8b77837894625d7118c997a57f7c4508f64c1664
alvinkatojr <alvinkatojr@gmail.com>
2017-05-01 19:33:17 +0300

    Adds strong params and callback method

--- engines/contacts/app/controllers/samurai/contacts/contacts_controller.rb
+++ engines/contacts/app/controllers/samurai/contacts/contacts_controller.rb
@@ -2,5 +2,15 @@ require_dependency "samurai/application_controller"
 
 module Samurai::Contacts
   class ContactsController < ApplicationController
+    before_action :set_contact, only: [:show, :edit, :update, :destroy]
+
+    private
+      def set_contact
+        @contact = Contact.find(params[:id])
+      end
+
+      def contact_params
+        params.require(:contact).permit(:first_item, :last_name, :company, :email, :phone, :user_id)
+      end
   end
 end
