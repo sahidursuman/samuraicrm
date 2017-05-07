16cdd76c82c6e0a440c31baf0c79fa5b473e1369
alvinkatojr <alvinkatojr@gmail.com>
2017-05-07 23:35:37 +0300

    Adds the register ability class method

--- engines/core/app/models/samurai/ability.rb
+++ engines/core/app/models/samurai/ability.rb
@@ -5,6 +5,15 @@ module Samurai
     class_attribute :abilities
     self.abilities = Set.new
 
+    # Allows us to go beyond the standard cancan initialize method which makes
+    # it difficult for engines to modify the default {Ability} of an
+    # application. The registered ability should behave properly as a
+    # stand-alone class and therefore should be easy to test in isolation.
+    # @param ability [Ability] a class that includes the CanCan::Ability module.
+    def self.register_ability(ability)
+      self.abilities.add(ability)
+    end
+
     def initialize(user)
 
       if user.admin?
